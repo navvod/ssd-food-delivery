3dd7c362c089c4bf8416f6546b1a9df7e25511a9
navvod <priyashanakila99@gmail.com>
2025-09-23 15:06:37 +0530

    csp done in the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,22 @@
-// src/App.js
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { CartProvider } from './context/CartContext';
 import { OrderProvider } from './context/OrderContext';
+import { Helmet } from 'react-helmet'; // Add react-helmet for CSP meta tags
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './index.css';
-//akila
+
+// akila
 import LoginPage from './pages/user/LoginPage';
 import RegisterPage from './pages/user/RegisterPage';
 import ProfilePage from './pages/user/ProfilePage';
 import CustomerSignUp from './pages/user/CustomerSignUp';
 import DeliverySignUp from './pages/user/DeliverySignUp';
-import RestaurantAdminSignUp from './pages/user/RestaurantAdminSignUp'; 
+import RestaurantAdminSignUp from './pages/user/RestaurantAdminSignUp';
 import ActiveOrderPage from './pages/order/ActiveOrderPage';
 
-
-//naduni
+// naduni
 import Home from './pages/restaurant/Home';
 import RestaurantsPage from './pages/restaurant/RestaurantsPage';
 import MenuPage from './pages/restaurant/MenuPage';
@@ -26,7 +26,7 @@ import AddMenuItemPage from './pages/restaurant/AddMenuItemPage';
 import EditMenuItemPage from './pages/restaurant/EditMenuItemPage';
 import AdminDashboard from './pages/restaurant/AdminDashboard';
 
-//lakna
+// lakna
 import AddToCartPage from './pages/order/AddToCartPage';
 import CartPage from './pages/order/CartPage';
 import EditCartPage from './pages/order/EditCartPage';
@@ -36,20 +36,33 @@ import OrderDetailsPage from './pages/order/OrderDetailsPage';
 import OrderHistoryPage from './pages/order/OrderHistoryPage';
 import AdminOrderPage from './pages/order/AdminOrderPage';
 
-
-//veenadhi
-
+// veenadhi
 import DriverMain from './pages/driver/DriverMain';
 import DriverProfile from './pages/driver/DriverProfile';
-import DriverRegistration from './pages/driver/DriverRegistration'; 
+import DriverRegistration from './pages/driver/DriverRegistration';
 
 function App() {
+  // Define a basic CSP for client-side rendering, aligned with server-side
+  const cspPolicy = `
+    default-src 'self';
+    script-src 'self' 'nonce-random123' https://trustedcdn.com;
+    style-src 'self' https://fonts.googleapis.com 'nonce-random123';
+    img-src 'self' https://images.example.com data:;
+    connect-src 'self' https://api.example.com http://localhost:5000;
+    font-src 'self' https://fonts.gstatic.com;
+    object-src 'none';
+    upgrade-insecure-requests;
+  `.replace(/\n/g, '');
+
   return (
     <AuthProvider>
       <CartProvider>
-       <OrderProvider>
-         <Router>
-           <Routes>
+        <OrderProvider>
+          <Helmet>
+            <meta httpEquiv="Content-Security-Policy" content={cspPolicy} />
+          </Helmet>
+          <Router>
+            <Routes>
               {/* akila */}
               <Route path="/login" element={<LoginPage />} />
               <Route path="/register" element={<RegisterPage />} />
@@ -77,18 +90,17 @@ function App() {
               <Route path="/orders" element={<OrderListPage />} />
               <Route path="/order/:orderId" element={<OrderDetailsPage />} />
               <Route path="/history" element={<OrderHistoryPage />} />
-              <Route path="/active-orders" element={<ActiveOrderPage/>} />
+              <Route path="/active-orders" element={<ActiveOrderPage />} />
               {/* Admin Routes */}
               <Route path="/admin-order" element={<AdminOrderPage />} />
 
-                {/* veenadhi */}
+              {/* veenadhi */}
               <Route path="/driver-main" element={<DriverMain />} />
               <Route path="/driver-profile" element={<DriverProfile />} />
               <Route path="/driver-registration" element={<DriverRegistration />} />
-
-           </Routes>
-         </Router>
-         <ToastContainer />
+            </Routes>
+          </Router>
+          <ToastContainer />
         </OrderProvider>
       </CartProvider>
     </AuthProvider>
